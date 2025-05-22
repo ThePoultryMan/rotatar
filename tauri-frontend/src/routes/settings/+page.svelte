b13941c326abc98e07a0eb61a396935d7081a413
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2025-05-22 16:57:42 -0400

    refactor: stylize and clarify settings page

--- tauri-frontend/src/routes/settings/+page.svelte
+++ tauri-frontend/src/routes/settings/+page.svelte
@@ -15,10 +15,19 @@
   });
 </script>
 
-{#if frontendData.state}
-  <select bind:value={audioDevice}>
-    {#each frontendData.state.audio_devices as device}
-      <option>{device}</option>
-    {/each}
-  </select>
-{/if}
+<div class="mx-3 mt-3">
+  <h1 class="mb-1 text-lg font-semibold">Settings</h1>
+  <hr class="mb-3" />
+  {#if frontendData.state}
+    <label for="audio-device">Audio Device: </label>
+    <select id="audio-device" bind:value={audioDevice}>
+      {#each frontendData.state.audio_devices as device}
+        <option>{device}</option>
+      {/each}
+    </select>
+    <p class="text-sm">
+      WARNING: Changing audio devices does not work well on all platform. If the app stops working
+      after changing devices, restart it. This will be fixed in a later version.
+    </p>
+  {/if}
+</div>
